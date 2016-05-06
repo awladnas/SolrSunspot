719e0b0371c75db91286d9543d514e323e19416b
awlad <awladliton@gmail.com>
2016-05-07 01:46:07 +0600

    added origin remote.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ Book.create! title: 'Walk with SCRUM', isbn: 'SC101', author: 'Schuill', publish
 Book.create! title: 'Walk with Image Processing', isbn: 'IP101', author: 'Jeff', published_at: Time.now
 Book.create! title: 'Walk with Background Job', isbn: 'BJ101', author: 'Robbert', published_at: Time.now
 Book.create! title: 'Walk with BrenchMarking', isbn: 'BM101', author: 'Doglus', published_at: Time.now
-puts "Seed Completed!"
 
+puts "Seed Completed!"
 puts "##############################################################################"
 puts " T H A N K  Y O U"
 puts "##############################################################################"
